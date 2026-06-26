97226cfae5d8026b7f1fb61232267a708793d723
rayanimanzi <r.mugangaim@alustudent.com>
2026-06-26 23:03:31 +0200

    Add python-import_modules tasks

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+argc = len(sys.argv) - 1
+if argc == 0:
+    print("0 arguments.")
+elif argc == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(argc))
+for i in range(1, argc + 1):
+    print("{}: {}".format(i, sys.argv[i]))
---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+result = 0
+for i in range(1, len(sys.argv)):
+    result += int(sys.argv[i])
+print(result)
