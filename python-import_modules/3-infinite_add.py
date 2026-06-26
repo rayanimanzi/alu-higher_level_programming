d063f251844a0004efcaf8a788be612664f7b9c2
rayanimanzi <r.mugangaim@alustudent.com>
2026-06-26 23:13:14 +0200

    Fix task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-result = 0
-for i in range(1, len(sys.argv)):
-    result += int(sys.argv[i])
-print(result)
+if __name__ == "__main__":
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print(result)
