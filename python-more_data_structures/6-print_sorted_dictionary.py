5479e158da163aaf8dc8079f7bcc7608e16d46c4
rayanimanzi <r.mugangaim@alustudent.com>
2026-07-09 21:18:41 +0200

    Fix stray character in 6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,5 +5,5 @@
 def print_sorted_dictionary(a_dictionary):
     """Print each key/value pair of a_dictionary sorted by key."""
     for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key]))o
+        print("{}: {}".format(key, a_dictionary[key]))
 
